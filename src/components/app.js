b62bd3f903691e7e2f8fbfad0de03a76e2462e5d
Yale Yuen <yaleyyuen@gmail.com>
2016-04-28 22:43:10 -0700

    start searchBar feature

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Component } from 'react';
 
+import SerachBar from '../containers/search_bar';
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <SerachBar />
     );
   }
 }
