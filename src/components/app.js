11b3520a4d18d36233062544827105d8a33d4d26
Yale Yuen <yaleyyuen@gmail.com>
2016-06-22 12:31:48 -0700

    implement weather_list container

--- src/components/app.js
+++ src/components/app.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Component } from 'react';
 
 import SerachBar from '../containers/search_bar';
+import WeatherList from '../containers/weather_list';
 
 export default class App extends Component {
   render() {
     return (
+      <div>
       <SerachBar />
+      <WeatherList />
+      </div>
     );
   }
 }
