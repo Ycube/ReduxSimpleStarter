0f6af05ab69c90c8b708ba97d843264db5cfd161
Yale Yuen <yaleyyuen@gmail.com>
2016-06-22 12:05:38 -0700

    complete weather_reducer

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,10 @@
-export default function(state=null,action){
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [],action){
+  switch (action.type) {
+    case FETCH_WEATHER :
+      // return state.concat([action.payload.data]); //use .concat instead of .push b/c push will mutate original state and that is what redux does not want to do
+      return [ acion.payload.data, ...state ]; //same as line 6 w/ ES6
+  }
   return state;
 }
