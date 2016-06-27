3ba882ae273dd5cbfe827e5ba54ec0514cb2017c
Yale Yuen <yaleyyuen@gmail.com>
2016-06-27 10:35:31 -0700

    implement google maps feature

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';  
+import GoogleMap from '../components/google_map';
 
 export default class WeatherList extends Component {
   renderWeather(cityData) {
@@ -8,10 +9,13 @@ export default class WeatherList extends Component {
     const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273);
     const pressure = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = cityData.city.coord; //es6 destructuring
+    // const lon = cityData.city.coord.lon;
+    // const lat = cityData.city.coord.lat;
 
     return (
       <tr key={name}>
-        <td>{name}</td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
         <td><Chart data={temps} color="orange" units="C"/></td>
         <td><Chart data={pressure} color="green" units="hPa"/></td>
         <td><Chart data={humidities} color="black" units="%"/></td>
